5aae35a0a8768b598240f6f6ad0706f8f0b1969c
alvarodelcachotena <alvarodct23@gmail.com>
2025-08-13 12:07:19 +0100

    responsive Section A & B

--- src/components/sections/SectionD.tsx
+++ src/components/sections/SectionD.tsx
@@ -32,7 +32,7 @@ const SectionD: React.FC<ContactSectionProps> = ({
   };
 
   return (
-    <div style={sectionStyle} className="flex items-center justify-center md:mt-[4%] lg:mt-[1%] w-[20%] ml-[7%] md:ml-[18%] md:w-[40%] lg:w-[70%] lg:ml-[39%] 2xl:w-[100%] 2xl:ml-[49%]">
+    <div style={sectionStyle} className="flex items-center justify-center md:mt-[4%] lg:mt-[1%] w-[20%] ml-[8%] md:ml-[18%] md:w-[40%] lg:w-[70%] lg:ml-[39%] 2xl:w-[85%] 2xl:ml-[46%]">
       <div
         style={contentStyle}
         className="w-full flex flex-col justify-center px-6 sm:px-8 md:px-12 mb-28 lg:mb-0 mx-auto"
